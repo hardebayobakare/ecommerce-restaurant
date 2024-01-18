16e009aae2bae11f075e2194195259831b987d1f
Adebayo Bakare <hardebayobakare@gmail.com>
2024-01-18 18:51:22 -0500

    Product card creation

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,15 +1,21 @@
 import getBillboard from "@/actions/get-billboards";
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
-    const billboard = await getBillboard("c4af6ea2-c3fc-4cfc-8916-3a99064a3424")
+    const billboard = await getBillboard("3b928bd5-b5a5-4a79-bf7a-d80e4c34cf39");
+    const products = await getProducts({ isFeatured: true});
     return ( 
         <Container>
             <div className="space-y-10 pb-10">
                 <Billboard data={billboard}/>
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products}/>
             </div>
         </Container>
      );
--- types.ts
+++ types.ts
@@ -9,3 +9,24 @@ export interface Category {
     name: string;
     billboard: Billboard
 }
+
+export interface Product {
+    id: string;
+    category: Category;
+    name: String;
+    price: string;
+    isFeatured: boolean;
+    size: Size;
+    images: Image[];
+}
+
+export interface Size {
+    id: string;
+    name: string;
+    value: string;
+}
+
+export interface Image {
+    id: string;
+    url: string;
+}
