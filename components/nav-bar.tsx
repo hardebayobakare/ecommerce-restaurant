3d4daf91d937417ddbd799056b3ea36a104cf968
Adebayo Bakare <hardebayobakare@gmail.com>
2024-01-18 00:13:58 -0500

    Navigation Bar Update

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,8 +1,10 @@
 import Container from "@/components/ui/container";
 import Link from "next/link";
 import MainNav from "@/components/main-nav";
+import getCategories from "@/actions/get-categories";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const categories = await getCategories();
     return (  
         <div className="border-b">
             <Container>
@@ -10,7 +12,7 @@ const Navbar = () => {
                     <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
                         <p className="font-bold text-xl">SIMPLYMARIAM RESTAURANT</p>
                     </Link>
-                    <MainNav data={[]}/>
+                    <MainNav data={categories} />
                 </div>
             </Container>
         </div>
